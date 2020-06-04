fe68227a253ee10effb43f1afbf10d386ecf4bb9
Mattias Hising <hising@gmail.com>
2020-06-04 19:59:46 +0200

    All the CRUD:s

--- src/index.js
+++ src/index.js
@@ -6,8 +6,14 @@ const initApp = async () => {
     // withCallback(onJson);
     // getWithPromise().then(onJson);
     // onJson(await getWithAsync());
-    const all = await itemStore.read();
-    console.log(all);
+    // const all = await itemStore.read();
+    const first = await itemStore.read(1);
+    // await itemStore.create({ name: "Ich bin einem neue item"});
+    //await itemStore.delete(2);
+    /* await itemStore.update(1, {
+        name: "Holy Smokessss"
+    }) */
+    console.log(first);
 };
 
 (async () => {
