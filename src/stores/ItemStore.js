b125bcbc675af93b92c1d763be93ae6a26b68a59
Mattias Hising <hising@gmail.com>
2020-06-05 12:49:50 +0200

    Prettier on ItemStore.js

--- src/stores/ItemStore.js
+++ src/stores/ItemStore.js
@@ -1,27 +1,27 @@
 import { HttpClient } from "../http/Client";
 
 class ItemStore {
-  constructor() {
-    this.client = new HttpClient("https://yetric.se/api");
-  }
+    constructor() {
+        this.client = new HttpClient("https://yetric.se/api");
+    }
 
-  async create (item) {
-    return await this.client.post("/sti/items", item);
-  }
+    async create(item) {
+        return await this.client.post("/sti/items", item);
+    }
 
-  async read (id = null) {
-    let path = id ? `/sti/items/${id}` : "/sti/items";
-    return await this.client.get(path);
-  }
+    async read(id = null) {
+        // TODO: Check that id is an integer
+        let path = id ? `/sti/items/${id}` : "/sti/items";
+        return await this.client.get(path);
+    }
 
-  async update (id, item) {
-    return await this.client.put(`/sti/items/${id}`, item);
-  }
+    async update(id, item) {
+        return await this.client.put(`/sti/items/${id}`, item);
+    }
 
-  async delete (id) {
-    return await this.client.delete(`/sti/items/${id}`);
-  }
-
+    async delete(id) {
+        return await this.client.delete(`/sti/items/${id}`);
+    }
 }
 
 export const itemStore = new ItemStore();
